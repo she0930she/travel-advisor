5bdd884941faf7507b6bd374f4afe7c0507cd661
she0930she <she0930she@hotmail.com>
2023-08-18 09:55:14 -0700

    remove rapid api option

--- src/api/index.js
+++ src/api/index.js
@@ -2,23 +2,6 @@ import axios from 'axios';
 
 const URL = 'https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary'
 
-
-const options = {
-  params: {
-    bl_latitude: '11.847676',
-    tr_latitude: '12.838442',
-    bl_longitude: '109.095887',
-    tr_longitude: '109.149359',
-
-  },
-  headers: {
-    'X-RapidAPI-Key': '8bde6bbfd9msh7b327765db5ae1cp16e82ejsn9456d8782d2c',
-    'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com'
-  }
-};
-
-
-
 export const getPlacesData = async (sw, ne) =>{
     //console.log("sw, ne", sw, ne)
     try{
